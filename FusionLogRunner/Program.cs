8a9cd7eb7087bdc246f5d5e157c416ec180cdeca
Szymon warda <maklipsa@gmail.com>
2017-02-25 13:29:46 +0100

    Added logging

--- FusionLogRunner/Program.cs
+++ FusionLogRunner/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace FusionLogRunner
 {
     class Program
     {
+        private static Logger logger = NLog.LogManager.GetLogger("Console");
         static void Main(string[] args)
         {
+            logger.Info("Started");
+            Console.ReadKey();
         }
     }
 }
